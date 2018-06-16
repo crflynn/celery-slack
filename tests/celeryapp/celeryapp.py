aad4aa4770b5733956e53deb2ff7681c7c6357d9
crflynn <crf204@gmail.com>
2018-06-16 13:42:09 -0400

    testing locally on celery

--- tests/celeryapp/celeryapp.py
+++ tests/celeryapp/celeryapp.py
@@ -10,35 +10,35 @@ from .schedule import get_schedule
 try:
     from ..secret import slack_webhook
 except Exception:
-    slack_webhook = os.environ['SLACK_WEBHOOK']
+    slack_webhook = os.environ["SLACK_WEBHOOK"]
 
 
-logging.basicConfig(level='INFO')
+logging.basicConfig(level="INFO")
 
 
 schedule = get_schedule()
 
-app = Celery('schedule')
-app.config_from_object('tests.celeryapp.config')
+app = Celery("schedule")
+app.config_from_object("tests.celeryapp.config")
 
 options = {
     "flower_base_url": "https://flower.example.com",
     "webhook": slack_webhook,
     "beat_schedule": schedule,
+    "show_beat": True,
     # "show_task_prerun": True,
     # "failures_only": True,
     # "show_celery_hostname": True,
     # "show_startup": False,
     # "show_shutdown": False,
-    "show_beat": True,
-    "beat_schedule": schedule,
+    "show_broker": True,
 }
 
-# logging.info('Creating celery-slack object.')
+# logging.info("Creating celery-slack object.")
 slack_app = Slackify(app, **options)
 # slack_app = Slackify(app, slack_webhook)
-# logging.info('Created celery-slack object.')
+# logging.info("Created celery-slack object.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.start()
--- tests/celeryapp/config.py
+++ tests/celeryapp/config.py
@@ -7,19 +7,19 @@ from .schedule import get_schedule, get_imports
 
 
 # General settings
-accept_content = ['json']
+accept_content = ["json"]
 
 
 # Time and date settings
 enable_utc = True
-timezone = 'UTC'
+timezone = "UTC"
 
 
 # Task settings
 # task_annotations = None
 # task_compression = None
 # task_protocol = 2
-task_serializer = 'json'
+task_serializer = "json"
 # task_publish_retry = True
 # task_publish_retry_policy = {}
 
@@ -39,9 +39,9 @@ task_serializer = 'json'
 
 
 # Task result backend settings
-result_backend = 'redis://localhost:6379'
-# result_backend = 'amqp://guest:guest@localhost:5672//'
-result_serializer = 'json'
+result_backend = "redis://localhost:6379"
+# result_backend = "amqp://guest:guest@localhost:5672//"
+result_serializer = "json"
 # result_compression = None
 result_expires = 172800
 # result_cache_max = False
@@ -72,32 +72,33 @@ redis_socket_timeout = 30
 # task_routes = None
 # worker_direct = False
 # task_create_missing_queues = True
-# task_default_queue = 'celery'
-# task_default_exchange = 'celery'
-# task_default_exchange_type = 'direct'
-# task_default_routing_key = 'celery'
-# task_default_delivery_mode = 'persistent'
+# task_default_queue = "celery"
+# task_default_exchange = "celery"
+# task_default_exchange_type = "direct"
+# task_default_routing_key = "celery"
+# task_default_delivery_mode = "persistent"
 
 
 # Broker settings
-broker_url = 'redis://localhost:6379'
-# broker_url = 'amqp://guest:guest@localhost:5672//'
+broker_url = "redis://localhost:6379"
+BROKER_URL = "redis://localhost:6379"
+# broker_url = "amqp://guest:guest@localhost:5672//"
 # broker_read_url = broker_url
 # broker_write_url = broker_url
-# broker_failover_strategy = 'round-robin'
+# broker_failover_strategy = "round-robin"
 # broker_use_ssl = False
 # broker_pool_limit = 10
 # broker_connection_timeout = 4.0
 # broker_connection_retry = True
 # broker_connection_max_retries = 100
-# broker_login_method = 'AMQPLAIN'
+# broker_login_method = "AMQPLAIN"
 # broker_transport_options = {}
 
 
 # Beat settings
 beat_schedule = get_schedule()
-# beat_scheduler = 'celery.beat:PersistentScheduler'
-# beat_schedule_filename = 'celerybeat-schedule'
+# beat_scheduler = "celery.beat:PersistentScheduler"
+# beat_schedule_filename = "celerybeat-schedule"
 # beat_sync_every = 0
 # beat_max_loop_interval = 0
 
@@ -119,8 +120,8 @@ worker_prefetch_multiplier = 1
 # Events settings
 # worker_send_task_events = False
 # task_send_sent_event = False
-# event_queue_prefix = 'celeryev'
-event_serializer = 'json'
+# event_queue_prefix = "celeryev"
+event_serializer = "json"
 
 
 # Remote Control commands
@@ -131,10 +132,10 @@ event_serializer = 'json'
 # Logging settings
 # worker_hijack_root_logger = True
 # worker_log_color = True
-# worker_log_format = '[%(asctime)s: %(levelname)s/%(processName)s] %(message)s'  # noqa
-# worker_task_log_format = '[%(asctime)s: %(levelname)s/%(processName)s][%(task_name)s(%(task_id)s)] %(message)s'  # noqa
+# worker_log_format = "[%(asctime)s: %(levelname)s/%(processName)s] %(message)s"  # noqa
+# worker_task_log_format = "[%(asctime)s: %(levelname)s/%(processName)s][%(task_name)s(%(task_id)s)] %(message)s"  # noqa
 # worker_redirect_stdouts = True
-# worker_redirect_stdouts_level = 'WARNING'
+# worker_redirect_stdouts_level = "WARNING"
 
 
 # Security options
@@ -144,8 +145,8 @@ event_serializer = 'json'
 
 
 # Custom component classes
-# worker_pool = 'prefork'
+# worker_pool = "prefork"
 # worker_pool_restarts = False
-# worker_autoscaler = 'celery.worker.autoscale:Autoscaler'
-# worker_consumer = 'celery.worker.consumer:Consumer'
-# worker_timer = 'kombu.async.hub.timer:Timer'
+# worker_autoscaler = "celery.worker.autoscale:Autoscaler"
+# worker_consumer = "celery.worker.consumer:Consumer"
+# worker_timer = "kombu.async.hub.timer:Timer"
